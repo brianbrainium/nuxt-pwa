1462ffae3b842b5872df6f4b2cfeb2df139d5db1
brianbrainium <brian-email@brainium.company>
2025-06-04 23:04:03 -0500

    Enable PWA module and prerender home

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  // Disable server-side rendering for SPA mode
   ssr: false,
-  modules: ['@vite-pwa/nuxt'],
+  modules: [
+    '@vite-pwa/nuxt',
+  ],
+  nitro: {
+    prerender: {
+      // Pre-render home page for offline use
+      routes: ['/'],
+    }
+  },
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
-      name: 'Nuxt\u00A03\u00A0PWA',
+      name: 'Nuxt 3 PWA',
       short_name: 'NuxtPWA',
-      description: 'Offline\u2011ready Nuxt\u00A03 app',
+      description: 'A Nuxt 3 Progressive Web App',
       theme_color: '#4A90E2',
       icons: [
         { src: '/pwa-icon-192x192.png', sizes: '192x192', type: 'image/png' },
